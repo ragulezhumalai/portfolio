86b31146fc4044a727afdcedb00c7e1b476ec479
Rahul <raghulelumalai@gmail.com>
2023-07-02 23:01:33 +0530

    fixes

--- app.js
+++ app.js
@@ -1,35 +1,19 @@
+//Menu bar action
 let burger= document.querySelector(".burger");
 let Links = document.querySelector(".nbar");
 
 burger.addEventListener('click',()=>{
   Links.classList.toggle("nbar-active")
 })
-
-
+// Theme changing code------
 const root = document.documentElement;
 const list = document.getElementsByClassName("theme");
-
-
-
-Array.from(list).forEach(function (selectElement) {
-  selectElement.addEventListener("change", function () {
-    console.log(this.value);
-    selectElement.style.setProperty("background-color",this.value)
-    root.style.setProperty("--primary", this.value);
-  });
-});
-
+// Default color
 root.style.setProperty("--primary", "rgba(140,0, 255, 0.996)");
 
-// practice
-
-function outer(n){
-  return function inner(){
-    return n++
-  }
-}
-
-let call=outer(5)
-console.log(call())
-console.log(call())
-console.log(call())
+Array.from(list).map((selected)=>{
+  selected.addEventListener("change",()=>{
+    console.log(selected.value)
+    // for changing BG-color of selected option.
+    selected.style.setProperty("background-color",selected.value)
+root.style.setProperty("--primary",selected.value);})})
